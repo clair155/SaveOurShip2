ec300945fa49592bcdf4ea15fc219ba8f305e73a
user <fldpi@tutanota.com>
2025-11-22 09:46:14 +0300

    Removed obsolete Royalty permits Harmony patch

--- Source/1.6/HarmonyPatches.cs
+++ Source/1.6/HarmonyPatches.cs
@@ -1479,16 +1479,6 @@ namespace SaveOurShip2
 		}
 	}*/
 
-	[HarmonyPatch(typeof(RoyalTitlePermitWorker), "AidDisabled")]
-	public static class RoyalTitlePermitWorkerInSpace
-	{
-		public static void Postfix(Map map, ref bool __result)
-		{
-			if (map != null && map.IsSpace() && __result == true)
-				__result = false;
-		}
-	}
-
 	[HarmonyPatch(typeof(Site), "PostMapGenerate")]
 	public static class RaidsStartEarly
 	{
@@ -5826,6 +5816,62 @@ namespace SaveOurShip2
 			return thing.GetRoom(allowedRegionTypes);
 		}
 	}
+
+	/*[HarmonyPatch(typeof(Pawn), "PreApplyDamage")]
+	public static class CheckDamage
+    {
+		public static void Prefix()
+		{
+			Log.Warning("PreApply DMG");
+		}
+    }*/
+
+	[HarmonyPatch(typeof(PawnRenderNodeWorker_Apparel_Head), "CanDrawNow")]
+	class HideHelmetsInside
+    {
+		public static bool Prefix(PawnDrawParms parms, ref bool __result)
+		{
+			if (parms.pawn != null && parms.pawn.Map != null)
+			{
+				Map map = parms.pawn.Map;
+				Room room = parms.pawn.Position.GetRoom(parms.pawn.Map);
+
+				bool isBreathable = !ShipInteriorMod2.ExposedToOutside(room) && map.GetComponent<ShipMapComp>().VecHasLS(parms.pawn.Position);
+				if (isBreathable) 
+				{
+					__result = false;
+					return false;
+				}
+			}
+			return true;
+        }
+    }
+
+	[HarmonyPatch(typeof(PawnRenderNodeWorker_Apparel_Body), "CanDrawNow")]
+	class HidesuitInside
+	{
+		public static bool Prefix(PawnRenderNode node, PawnDrawParms parms, ref bool __result)
+		{
+			if (parms.pawn != null && parms.pawn.Map != null)
+			{
+				Map map = parms.pawn.Map;
+				Room room = parms.pawn.Position.GetRoom(parms.pawn.Map);
+
+				bool isBreathable = !ShipInteriorMod2.ExposedToOutside(room) && map.GetComponent<ShipMapComp>().VecHasLS(parms.pawn.Position);
+				if (isBreathable)
+                {
+                    bool isSuit = node.apparel?.def.defName.Contains("Suit") ?? false;
+					if (isSuit)
+					{
+						__result = false;
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
+
 
 	/*[HarmonyPatch(typeof(ActiveDropPod),"PodOpen")]
 	public static class ActivePodFix{
