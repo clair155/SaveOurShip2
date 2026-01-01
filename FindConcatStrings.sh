ee6d9f72b418f202d160fa24da3729612c552ac1
user <fldpi@tutanota.com>
2026-01-01 12:04:47 +0300

    Added comments to scripts

--- FindConcatStrings.sh
+++ FindConcatStrings.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
+# Searches for string concatenation which are likely
+# to be internationalization error because compared to string with format
+# parameter, order of words and numbers is not parameterized and can be wrong
+# for Asian langages etc.
+# The output of the script is filtered so it gives few enough results, to
+# manullay review all of them. But may be not covering some cases.
+# Work pass on report by that script was already done.
+
 find ./Source/1.6 -name "*.cs" | xargs grep -nHP '\" \+|\"\+|\+\"|\+ \"' | grep -Ev "SoS.|TranslatorFormattedStringExtensions|.Colorize\(|Log.|Things/|UI/|ThingDef.Named|HarmonyPatch\(|BaseGen.symbolStack.Push" > ConcatStrinReport.txt
