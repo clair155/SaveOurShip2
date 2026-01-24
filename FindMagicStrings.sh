85d3ea6eac542471904054c297ae45f4d2e95d75
user <fldpi@tutanota.com>
2026-01-24 22:37:35 +0300

    Thanks to previous work by Rim World Mod Korean, trying to finds shorter magic strings with slightly better filter resulted in over dozen of small untranslated strings found in about 400-like report.

--- FindMagicStrings.sh
+++ FindMagicStrings.sh
@@ -5,4 +5,4 @@
 # Result is filtered and short enough to be worked manually on.
 # One work pass on results of this script was alreafy done.
 
-find ./Source/1.6 -name "*.cs" | xargs grep -nHP '"[^"]{26,}"' | grep -Ev "SoS.|TranslatorFormattedStringExtensions|.Colorize\(|Log.|Things/|UI/|ThingDef.Named|HarmonyPatch\(|BaseGen.symbolStack.Push" > MagicStrinReport.txt
+find ./Source/1.6 -name "*.cs" | xargs grep -nHP '"[^"]{13,}"' | grep -Ev "Scribe_|SoS.|TranslatorFormattedStringExtensions|.Colorize\(|Log.|Things/|UI/|ThingDef.Named|HarmonyPatch\(|BaseGen.symbolStack.Push" > MagicStrinReport.txt
